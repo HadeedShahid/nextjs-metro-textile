78a2c9fc10044cbe170ad9b1ecda89d04a765cc2
HadeedShahid <hadeed.shahid08@gmail.com>
2026-03-20 18:02:47 +0500

    feat: introduce a new Sheet UI component, update the font system, and switch to Lucide icons.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import CoreProducts from "@/components/CoreProducts";
-import Cta from "@/components/Cta";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import OurClient from "@/components/OurClient";
-import TrustedProducts from "@/components/TrustedProducts";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <CoreProducts />
-      <TrustedProducts />
-      <OurClient />
-      <Cta />
+      Hello
+
     </>
   );
 }
